bd42e374a77e36dd069f16831d41901e24d14905
Korjubzot <korjubzot@gmail.com>
2023-09-17 22:11:59 +0900

    Firebase auth fully functional

--- src/pages/UserAccount.js
+++ src/pages/UserAccount.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Signin from "../components/auth/Signin";
 import Signup from "../components/auth/Signup";
+import AuthDetails from "../components/auth/authDetails";
 
 function UserAccount() {
   return (
     <div>
       <h1>Create Account</h1>
       <Signin />
+      <AuthDetails />
       <Signup />
     </div>
   );
