b6a378b6122ba13f993ff7ae442ea60e88392fec
Korjubzot <korjubzot@gmail.com>
2023-09-17 15:59:37 +0900

    Initial Firebase setup

---
+++ src/components/Signup.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { navLink, useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+
+function Signup() {
+  return <div>Hello</div>;
+}
---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCGN1U7wpjIFT65zbhMxoCnCGbGkZRw0Cc",
+  authDomain: "dlicious-22926.firebaseapp.com",
+  projectId: "dlicious-22926",
+  storageBucket: "dlicious-22926.appspot.com",
+  messagingSenderId: "927600432622",
+  appId: "1:927600432622:web:43e1d6ff9784cdaa2baaff",
+  measurementId: "G-QGLFNMV94E",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
