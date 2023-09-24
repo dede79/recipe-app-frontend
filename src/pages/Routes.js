235d15d3f3185bc82c90e1f5f6ab9cd20e9b30c3
Korjubzot <korjubzot@gmail.com>
2023-09-24 10:38:11 +0900

    Auth now redirects signed out users to signup correctly

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,18 +1,24 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./Home";
 import UserRecipes from "./UserRecipes";
 import UserAccount from "./UserAccount";
 import Signin from "../components/auth/Signin";
+import { auth } from "../firebase";
 
 function RoutesConfig() {
+  const user = auth.currentUser;
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/usercreateaccount" element={<UserAccount />} />
       <Route path="/recipes" element={<Home />} />
-      <Route path="/userrecipes" element={<UserRecipes />} />
-      <Route path="/signin" element={<Signin />} />
+      <Route
+        path="/userrecipes"
+        element={user ? <UserRecipes /> : <Navigate to="/signin" />}
+      />
+      <Route path="/signin" element={user ? <Navigate to="/" /> : <Signin />} />
       {/* Add more routes here */}
     </Routes>
   );
