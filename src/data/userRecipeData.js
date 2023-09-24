ec79dff31c77a395513695e806adf5b5936a1f80
Korjubzot <korjubzot@gmail.com>
2023-09-24 11:23:36 +0900

    Added recipe slider in userRecipes component

---
+++ src/data/userRecipeData.js
@@ -0,0 +1,10 @@
+const userRecipes = [
+  { id: 1, title: "Spaghetti Carbonara", cuisine: "Italian" },
+  { id: 2, title: "Sushi Roll", cuisine: "Japanese" },
+  { id: 3, title: "Chicken Tikka Masala", cuisine: "Indian" },
+  { id: 4, title: "Taco Salad", cuisine: "Mexican" },
+  { id: 5, title: "Pad Thai", cuisine: "Thai" },
+  { id: 6, title: "Greek Salad", cuisine: "Greek" },
+];
+
+export default userRecipes;
--- src/pages/UserRecipes.js
+++ src/pages/UserRecipes.js
@@ -1,7 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+
+// sample data for user recipes
+// this will later become a database link
+import userRecipes from "../data/userRecipeData";
 
 const UserRecipes = () => {
-  return <h1>userRecipes</h1>;
+  return (
+    <div className="user-recipes-container">
+      <h1>Your Recipes!</h1>
+      <div>
+        <Splide options={{ perPage: 3, gap: "1rem" }}>
+          {userRecipes.map((recipe, index) => (
+            <SplideSlide key={index}>
+              {/* this makes the slide clickable but doesn't lead anywhere yet 
+              currently it links to the recipes id which might work fine, actually */}
+              <a href={`/recipe/${recipe.id}`}>
+                <RecipeSlide recipe={recipe} />
+              </a>
+            </SplideSlide>
+          ))}
+        </Splide>
+      </div>
+    </div>
+  );
+};
+
+const RecipeSlide = ({ recipe }) => {
+  return (
+    <div className="recipe-slide">
+      <h2>{recipe.title}</h2>
+      <p>Cuisine: {recipe.cuisine}</p>
+      {/* more details or styling here */}
+    </div>
+  );
 };
 
 export default UserRecipes;
