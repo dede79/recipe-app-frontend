45129ba1739d99ffe0ced7f883b2ed06ae62659f
Korjubzot <korjubzot@gmail.com>
2023-10-02 12:46:16 +0100

    Updated Homepage with a user recipe slider

---
+++ src/components/userRecipes.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+
+// sample data for user recipes
+// this will later become a database link
+
+import userRecipes from "./data/userRecipeData";
+
+const UserRecipes = () => {
+  return (
+    <div className="user-recipes-container">
+      <h1>Your Recipes!</h1>
+      <div>
+        <Splide options={{ perPage: 3, gap: "1rem" }}>
+          {userRecipes.map((recipe, index) => (
+            <SplideSlide key={index}>
+              {/* this makes the slide clickable but doesn't lead anywhere yet. 
+              currently it links to the recipes id which might work fine, actually */}
+              <a href={`/recipe/${recipe.id}`}>
+                <RecipeSlide recipe={recipe} />
+              </a>
+            </SplideSlide>
+          ))}
+        </Splide>
+      </div>
+      <Link to="/add-new-recipe">Add a new recipe</Link>
+    </div>
+  );
+};
+
+const RecipeSlide = ({ recipe }) => {
+  return (
+    <div className="recipe-slide">
+      <h2>{recipe.title}</h2>
+      <p>Cuisine: {recipe.cuisine}</p>
+      {/* more details or styling here */}
+    </div>
+  );
+};
+
+export default UserRecipes;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Popular from "../components/Popular";
 import Veggie from "../components/Veggie";
+import UserRecipes from "../components/userRecipes";
 
-function Home(){
-  return(
-      <div>
-        <Popular />
-        <Veggie />
-      </div>
-  )
+function Home() {
+  return (
+    <div>
+      <Popular />
+      <UserRecipes />
+      <Veggie />
+    </div>
+  );
 }
 
 export default Home;
